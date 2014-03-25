6d2620d528a83b321294e84b2f4d7c0f198d9661
kamijo <kamijo@ninja.co.jp>
2014-03-26 08:01:43 +0900

    Updated submodule hoedown

--- autogen.sh
+++ autogen.sh
@@ -14,7 +14,7 @@ if [ "$1" = "clean" ]; then
     echo "Cleanup"
     rm -rf .libs autom4te*.cache scripts aclocal.m4 configure config.log config.status config.guess config.sub .deps stamp-h1 depcomp install-sh ltmain.sh missing libtool config.h config.h.in* m4 ar-lib
     rm -f *.o *.la *.lo *.slo Makefile.in Makefile
-    rm -rf hoedown/src/.deps hoedown/src/.dirstamp
+    rm -rf hoedown/src/.deps hoedown/src/.dirstamp hoedown/src/*.lo
     exit
 fi
 
